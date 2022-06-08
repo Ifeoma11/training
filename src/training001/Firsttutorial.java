b301413637dd515da02cdfe20c39396a5b5b4ca7
ifeoma <ifeoma.opiah@gmail.com>
2022-06-08 19:04:08 +0200

    ohs-101

---
+++ src/training001/Firsttutorial.java
@@ -0,0 +1,15 @@
+package training001;
+
+public class Firsttutorial {
+   public static void main ( String [] args ){
+       //Program to add numbers
+       int a = 33;
+       int b = 69;
+       int c = a+b;
+
+       System.out.println (c);
+
+
+
+   }
+}
